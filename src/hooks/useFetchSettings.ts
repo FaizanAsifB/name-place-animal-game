13ffca606a821204ee34a85ec4e8889452bb10a3
faizan <faizanasif.dev@gmail.com>
2023-12-29 22:23:55 +0100

    build error

--- src/hooks/useFetchSettings.ts
+++ src/hooks/useFetchSettings.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
 import { fetchLobbyData } from '../utils/fetchData'
+import { GameSettings } from '../lib/types'
 
 export const useFetchSettings = () => {
   const params = useParams()
@@ -8,6 +9,7 @@ export const useFetchSettings = () => {
 
   return useQuery({
     queryKey: ['settings', roomId],
-    queryFn: ({ queryKey }) => fetchLobbyData(queryKey[1], 'lobbies'),
+    queryFn: ({ queryKey }) =>
+      fetchLobbyData<GameSettings>(queryKey[1], 'lobbies'),
   })
 }
