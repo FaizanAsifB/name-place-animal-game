33ebd57afbe202db2c1017921e6831ec512ffc30
FaizanAsifB <faizanasif.dev@gmail.com>
2024-01-14 19:18:30 +0100

    disabled visual on

--- src/pages/GameRoom/components/AlphabetsScroll.tsx
+++ src/pages/GameRoom/components/AlphabetsScroll.tsx
@@ -3,9 +3,9 @@ import { Dialog, DialogContent } from '@/components/ui/dialog'
 import 'slick-carousel/slick/slick-theme.css'
 import 'slick-carousel/slick/slick.css'
 
+import { GameStates } from '@/lib/types'
 import { useEffect, useState } from 'react'
 import AlphabetsSlider from './AlphabetsSlider'
-import { GameStates } from '@/lib/types'
 
 type AlphabetsScrollProps = {
   gameState: GameStates | undefined
@@ -21,7 +21,7 @@ const AlphabetsScroll = ({ gameState }: AlphabetsScrollProps) => {
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent visual={true}>
+      <DialogContent /* visual={true} */>
         <AlphabetsSlider setOpen={setOpen} gameState={gameState} />
       </DialogContent>
     </Dialog>
