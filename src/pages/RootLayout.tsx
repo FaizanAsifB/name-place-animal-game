16dc5d8bf9ca7b524daadc1fdaf01b79ae21e9c5
FaizanAsifB <faizanasif.dev@gmail.com>
2024-02-02 08:20:36 +0100

    removed modal div

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,11 +1,11 @@
-import { Toaster } from '@/components/ui/toaster'
 import { Toaster as Sonner } from '@/components/ui/sonner'
+import { Toaster } from '@/components/ui/toaster'
 
 import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
   return (
-    <div className="bg-cover bg-main-bg font-display">
+    <div className="bg-cover bg-main-bg">
       <main className="flex min-h-screen p-4 md:p-6 xl:container lg:p-8 ">
         <div className="flex flex-col flex-1 lg:border-main-border lg:border-2 lg:shadow-main lg:px-8 lg:rounded-lg">
           <Outlet />
