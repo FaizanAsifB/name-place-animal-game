531e9dc56ca6a931dc462da51537bad368a701c9
FaizanAsifB <faizanasif.dev@gmail.com>
2024-02-07 08:38:57 +0100

    responsive skeleton

--- src/pages/GameCreation/components/CreationSkeleton.tsx
+++ src/pages/GameCreation/components/CreationSkeleton.tsx
@@ -3,9 +3,9 @@ import { Skeleton } from '@/components/ui/skeleton'
 const CreationSkeleton = () => {
   return (
     <div className="flex flex-col flex-1 p-6 space-y-6">
-      <Skeleton className=" h-[100px] " />
+      <Skeleton className=" h-[100px]" />
       <div>
-        <Skeleton className=" h-[50px] mx-auto w-1/5" />
+        <Skeleton className=" h-[50px] mx-auto w-[15ch]" />
 
         <div className="mt-8 space-y-10">
           <div className="space-y-6 text-center lg:text-start lg:grid lg:grid-cols-2 lg:items-center">
@@ -16,7 +16,7 @@ const CreationSkeleton = () => {
               </div>
               <Skeleton className="hidden text-lg lg:block h-[24px] w-[30ch]" />
             </div>
-            <Skeleton className="hidden text-lg lg:block h-[24px]" />
+            <Skeleton className="h-[24px]" />
           </div>
           <div className="space-y-6 text-center lg:text-start lg:grid lg:grid-cols-2 lg:items-center">
             <div className="lg:grid lg:gap-2">
@@ -26,7 +26,7 @@ const CreationSkeleton = () => {
               </div>
               <Skeleton className="hidden text-lg lg:block h-[24px] w-[30ch]" />
             </div>
-            <Skeleton className="hidden text-lg lg:block h-[24px]" />
+            <Skeleton className="h-[24px]" />
           </div>
           <div className="space-y-6 text-center lg:text-start lg:grid lg:grid-cols-2 lg:items-center">
             <div className="lg:grid lg:gap-2">
@@ -36,7 +36,7 @@ const CreationSkeleton = () => {
               </div>
               <Skeleton className="hidden text-lg lg:block h-[24px] w-[30ch]" />
             </div>
-            <Skeleton className="hidden text-lg lg:block h-[24px]" />
+            <Skeleton className="h-[24px]" />
           </div>
           <div className="space-y-6 text-center lg:text-start lg:grid lg:grid-cols-2 lg:items-center">
             <div className="lg:grid lg:gap-2">
@@ -46,7 +46,7 @@ const CreationSkeleton = () => {
               </div>
               <Skeleton className="hidden text-lg lg:block h-[24px] w-[30ch]" />
             </div>
-            <Skeleton className="hidden text-lg lg:block h-[24px]" />
+            <Skeleton className="h-[24px]" />
           </div>
           <div className="space-y-6 text-center lg:text-start lg:grid lg:grid-cols-2 lg:items-center">
             <div className="lg:grid lg:gap-2">
@@ -56,7 +56,7 @@ const CreationSkeleton = () => {
               </div>
               <Skeleton className="hidden text-lg lg:block h-[24px] w-[30ch]" />
             </div>
-            <Skeleton className="hidden text-lg lg:block h-[24px]" />
+            <Skeleton className="h-[24px]" />
           </div>
           <div className="space-y-6 text-center lg:text-start lg:grid lg:grid-cols-2 lg:items-center">
             <div className="lg:grid lg:gap-2">
@@ -66,20 +66,10 @@ const CreationSkeleton = () => {
               </div>
               <Skeleton className="hidden text-lg lg:block h-[24px] w-[30ch]" />
             </div>
-            <Skeleton className="hidden text-lg lg:block h-[24px]" />
-          </div>
-          <div className="space-y-6 text-center lg:text-start lg:grid lg:grid-cols-2 lg:items-center">
-            <div className="lg:grid lg:gap-2">
-              <div className="flex items-center justify-center gap-1 lg:justify-start ">
-                <Skeleton className=" h-[24px] w-[24px] " />
-                <Skeleton className=" h-[24px] w-[12ch] " />
-              </div>
-              <Skeleton className="hidden text-lg lg:block h-[24px] w-[30ch]" />
-            </div>
-            <Skeleton className="hidden text-lg lg:block h-[24px]" />
+            <Skeleton className="h-[24px]" />
           </div>
         </div>
-        <Skeleton className="h-12 w-[12ch] mx-auto my-6" />
+        <Skeleton className="h-12 w-[12ch] mx-auto my-12" />
       </div>
     </div>
   )
--- src/pages/GameScreen/components/GameSkeleton.tsx
+++ src/pages/GameScreen/components/GameSkeleton.tsx
@@ -3,11 +3,8 @@ import { Skeleton } from '@/components/ui/skeleton'
 const GameSkeleton = () => {
   return (
     <>
-      <div className="flex justify-between py-8 ">
-        <Skeleton className="h-[100px] w-[100px]" />
-        <Skeleton className="h-[100px] w-[200px]" />
-        <Skeleton className="h-[100px] w-[100px]" />
-      </div>
+      <Skeleton className="h-[100px]" />
+
       <div className="pt-6 pb-8 md:pb-12 lg:pb-16 lg:pt-8">
         <div className="grid gap-8 md:gap-10 md:grid-cols-2 lg:grid-cols-3 lg:gap-x-12 lg:grid-rows-3">
           <Skeleton className="w-full h-[250px]" />
